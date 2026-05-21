622f72b41a19a652694f2412406440c6e59b6cfa
lela <aryelaribeiro014@gmail.com>
2026-05-21 15:43:26 -0300

    Initial commit

--- src (abrir com vs/Sample.js
+++
@@ -1,8 +0,0 @@
-export function sampleWithReplacement(data, size) {
-    const result = [];
-    for (let i = 0; i < size; i++) {
-        const idx = Math.floor(Math.random() * data.length);
-        result.push(data[idx]);
-    }
-    return result;
-}
